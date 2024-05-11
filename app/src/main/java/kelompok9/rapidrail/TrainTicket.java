1869b09139be41b104a56b2c637c58e2b1d936a4
Naufalpc11 <146335947+Naufalpc11@users.noreply.github.com>
2024-05-11 17:34:58 +0800

    Update and rename TrainTicket to TrainTicket.java

---
+++ app/src/main/java/kelompok9/rapidrail/TrainTicket.java
@@ -0,0 +1,41 @@
+package kelompok9.rapidrail;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TrainTicket extends Ticket{
+    
+    private String costumerName;
+    public TrainTicket(String costumerName, String ticketId, double price) {
+        super(ticketId, price);
+        this.costumerName = costumerName;
+    }
+
+    public void setCostumerName(String costumerName){
+        this.costumerName = costumerName;
+    }
+    public String getCostumerName() {
+        return costumerName;
+    }
+
+    @Override
+    public String printTicket() {
+        String fileName = "train_ticket_" + getTicketId() + ".txt";
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                String ticketInfo = "Ticket ID: " + getTicketId() + "\n";
+                ticketInfo += "Customer Name: " + getCostumerName() + "\n";
+                ticketInfo += "Price: $" + getPrice() + "\n";
+
+                writer.write(ticketInfo); // Menulis informasi tiket ke file
+
+                System.out.println("Ticket details written to file: " + fileName);
+                return ticketInfo;
+            } catch (IOException e) {
+                System.out.println("Error writing ticket details to file: " + e.getMessage());
+                return null;
+            }
+            }
+
+}
