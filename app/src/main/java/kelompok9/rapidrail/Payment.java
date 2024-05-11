e3de9724ff8299a9e2a6994df9fb3e44aec8b66f
Naufalpc11 <146335947+Naufalpc11@users.noreply.github.com>
2024-05-11 14:04:00 +0800

    Update Payment.java

--- app/src/main/java/kelompok9/rapidrail/Payment.java
+++ app/src/main/java/kelompok9/rapidrail/Payment.java
@@ -1,26 +1,29 @@
 package kelompok9.rapidrail;
 
-public class Payment {
-    private String purchaseMethod;
+public class Payment extends Ticket {
+    private PurchaseMethod purchaseMethod;
     private String atmNumber;
     private int atmPin;
 
-    Payment(String purchaseMethod, String atmNumber, int atmPin){
+    public Payment(PurchaseMethod purchaseMethod, String atmNumber, int atmPin, double price){
+        super("N/A", price);
         this.purchaseMethod = purchaseMethod;
         this.atmNumber = atmNumber;
         this.atmPin = atmPin;
     }
 
-    public void setPurchaseMethod(String purchaseMethod){
+    public void setPurchaseMethod(PurchaseMethod purchaseMethod){
         this.purchaseMethod = purchaseMethod;
     }
-    public String getPurchaseMethod(){
+
+    public PurchaseMethod getPurchaseMethod(){
         return purchaseMethod;
     }
 
     public void setAtmNumber(String atmNumber){
         this.atmNumber = atmNumber;
     }
+
     public String getAtmNumber(){
         return atmNumber;
     }
@@ -28,7 +31,19 @@ public class Payment {
     public void setAtmPin(int atmPin){
         this.atmPin = atmPin;
     }
+
     public int getAtmPin(){
         return atmPin;
+    }
+
+    public void showPaymentDetails() {
+        System.out.println("Purchase Method: " + purchaseMethod);
+        System.out.println("ATM Number: " + atmNumber);
+        System.out.println("Total Price : " );
+    }
+
+    @Override
+    public String printTicket() {
+        throw new UnsupportedOperationException("Unimplemented method 'printTicket'");
     }
 }
