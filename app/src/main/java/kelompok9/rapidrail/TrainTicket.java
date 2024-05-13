b6af77f7ac34102e3bb925e4974a0fb674cd2502
Naufalpc11 <146335947+Naufalpc11@users.noreply.github.com>
2024-05-13 13:36:10 +0800

    Update TrainTicket.java

--- app/src/main/java/kelompok9/rapidrail/TrainTicket.java
+++ app/src/main/java/kelompok9/rapidrail/TrainTicket.java
@@ -1,18 +1,14 @@
 package kelompok9.rapidrail;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class TrainTicket extends Ticket{
-    
     private String costumerName;
-    public TrainTicket(String costumerName, String ticketId, double price) {
+
+    public TrainTicket(String ticketId, double price, String costumerName) {
         super(ticketId, price);
         this.costumerName = costumerName;
     }
 
-    public void setCostumerName(String costumerName){
+    public void setCostumerName(String costumerName) {
         this.costumerName = costumerName;
     }
     public String getCostumerName() {
@@ -20,22 +16,8 @@ public class TrainTicket extends Ticket{
     }
 
     @Override
-    public String printTicket() {
-        String fileName = "train_ticket_" + getTicketId() + ".txt";
-
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-                String ticketInfo = "Ticket ID: " + getTicketId() + "\n";
-                ticketInfo += "Customer Name: " + getCostumerName() + "\n";
-                ticketInfo += "Price: $" + getPrice() + "\n";
-
-                writer.write(ticketInfo);
-
-                System.out.println("Ticket details written to file: " + fileName);
-                return ticketInfo;
-            } catch (IOException e) {
-                System.out.println("Error writing ticket details to file: " + e.getMessage());
-                return null;
-            }
-            }
-
+    public void printInfotmation() {
+        System.out.println("Hello");
+    }
+    
 }
