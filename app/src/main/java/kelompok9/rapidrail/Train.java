9a91a6d7ccbc7c0799418e08f4523d9e88dbbe3c
Naufalpc11 <146335947+Naufalpc11@users.noreply.github.com>
2024-05-20 18:31:54 +0800

    Update Train.java

--- app/src/main/java/kelompok9/rapidrail/Train.java
+++ app/src/main/java/kelompok9/rapidrail/Train.java
@@ -1,44 +1,76 @@
 package kelompok9.rapidrail;
 
-public class Train {
-    private String trainId;
-    private String trainName;
-    private int seatSlot;
-    private String seatType;
+import java.util.ArrayList;
+import java.util.List;
 
-    Train(String trainId, String trainName, int seatSlot, String seatType) {
-        this.trainId = trainId;
-        this.trainName = trainName;
-        this.seatSlot = seatSlot;
-        this.seatType = seatType;
+
+public class Schedule {
+    private List<TrainSchedule> trainSchedules;
+
+    public Schedule() {
+        this.trainSchedules = new ArrayList<>();
     }
 
-    public void setTrainId(String trainId){
-        this.trainId = trainId;
-    }
-    public String getTrainId(){
-        return trainId;
+    public void addTrainSchedule(Train train, String departureStation, String departureTime, String arrivalStation, String arrivalTime) {
+        TrainSchedule trainSchedule = new TrainSchedule(train, departureStation, departureTime, arrivalStation, arrivalTime);
+        trainSchedules.add(trainSchedule);
     }
 
-    public void setTrainName(String trainName){
-        this.trainName = trainName;
-    }
-    public String getTrainName(){
-        return trainName;
-    }
-
-    public void setSeatSlot(int seatSlot){
-        this.seatSlot = seatSlot;
-    }
-    public int getSeatSlot(){
-        return seatSlot;
+    public void displayAllTrainSchedules() {
+        System.out.println("All Train Schedules:");
+        for (TrainSchedule schedule : trainSchedules) {
+            System.out.println(schedule);
+        }
     }
 
-    public void setSeatType(String seatType){
-        this.seatType = seatType;
+    public static class TrainSchedule {
+        private Train train;
+        private String departureStation;
+        private String departureTime;
+        private String arrivalStation;
+        private String arrivalTime;
+
+        public TrainSchedule(Train train, String departureStation, String departureTime, String arrivalStation, String arrivalTime) {
+            this.departureStation = departureStation;
+            this.departureTime = departureTime;
+            this.arrivalStation = arrivalStation;
+            this.arrivalTime = arrivalTime;
+        }
+
+    public void setDepartureStation(String departureStation) {
+        this.departureStation = departureStation;
     }
-    public String getSeatType(){
-        return seatType;
+    public String getDepartureStation() {
+        return departureStation;
     }
 
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setArrivalStation(String arrivalStation) {
+        this.arrivalStation = arrivalStation;
+    }
+    public String getArrivalStation() {
+        return arrivalStation;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    
+
+        public String showSchedule() {
+            return "Train: " + train.getTrainName() + " (ID: " + train.getTrainId() + ")"
+                    + ", Departure Station: " + getDepartureStation() + ", Departure Time: " + getDepartureTime()
+                    + ", Arrival Station: " + getArrivalStation() + ", Arrival Time: " + getArrivalTime();
+        }
+    }
 }
